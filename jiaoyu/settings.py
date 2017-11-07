8836d0542aa5a1d0c64d01f21d9eff4d53a8b4ac
keepingoner <keepingoner@163.com>
2017-11-07 11:24:33 +0800

    修改中文

--- jiaoyu/settings.py
+++ jiaoyu/settings.py
@@ -123,7 +123,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-LANGUAGE_CODE = 'zh-Hans'
+LANGUAGE_CODE = 'zh-hans'
 
 TIME_ZONE = 'Asia/Shanghai'
 
